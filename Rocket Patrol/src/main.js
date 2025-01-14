7e6b60173e19c857dafc80682c964a52c58a0041
DylanP2027 <dkparas@ucsc.edu>
2025-01-13 19:58:15 -0800

    Add files via upload

---
+++ Rocket Patrol/src/main.js
@@ -0,0 +1,7 @@
+let config = { // The configurations for the size of the window.
+    type: Phaser.AUTO, // The graphics context to render the game in, Phaser will choose for you since it's set to AUTO
+    width: 640,
+    height: 480
+  }
+
+let game = new Phaser.Game(config) // Sets up the new phaser game.
