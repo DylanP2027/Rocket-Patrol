fc046cb49c4dcad670e08107c3c1e8b6c5b236f8
DylanP2027 <dkparas@ucsc.edu>
2025-01-14 21:00:27 -0800

    Delete messed up Rocket Patrol directory

--- Rocket Patrol/src/main.js
+++
@@ -1,7 +0,0 @@
-let config = { // The configurations for the size of the window.
-    type: Phaser.AUTO, // The graphics context to render the game in, Phaser will choose for you since it's set to AUTO
-    width: 640,
-    height: 480
-  }
-
-let game = new Phaser.Game(config) // Sets up the new phaser game.
